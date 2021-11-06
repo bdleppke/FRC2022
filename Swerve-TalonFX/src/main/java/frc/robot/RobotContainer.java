99d658f3a7b48d02240d152a382703c87d42a9fb
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2021-11-06 19:13:05 -0400

    Fixed RunCommand requirements

--- Swerve-TalonFX/src/main/java/frc/robot/RobotContainer.java
+++ Swerve-TalonFX/src/main/java/frc/robot/RobotContainer.java
@@ -46,12 +46,16 @@ public class RobotContainer {
 
 		// Configure default commands
 		// Set the default drive command to split-stick arcade drive
+		RunCommand defaultCmd = new RunCommand(() -> m_robotDrive.drive(m_driverController.getY(GenericHID.Hand.kLeft),
+		m_driverController.getX(GenericHID.Hand.kRight), m_driverController.getX(GenericHID.Hand.kLeft),
+		false));
+		defaultCmd.addRequirements(m_robotDrive);
+
 		m_robotDrive.setDefaultCommand(
 				// A split-stick arcade command, with forward/backward controlled by the left
 				// hand, and turning controlled by the right.
-				new RunCommand(() -> m_robotDrive.drive(m_driverController.getY(GenericHID.Hand.kLeft),
-						m_driverController.getX(GenericHID.Hand.kRight), m_driverController.getX(GenericHID.Hand.kLeft),
-						false)));
+				defaultCmd
+		);
 	}
 
 	/**
